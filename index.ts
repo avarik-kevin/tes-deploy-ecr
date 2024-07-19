d222f095d2cfb3937ce84d67e118859b8474599b
Kevin Tanudjaja <kevintan.me@gmail.com>
2024-07-19 17:02:59 +0700

    add dotenv

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import 'dotenv/config';
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello ${process.env.ABC}`)
 })
 
 app.listen(port, () => {
